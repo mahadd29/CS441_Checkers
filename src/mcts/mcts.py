95e95139ce65f918be22ba4bebf1bf1db6511383
mahadd29 <mahadd29@users.noreply.github.com>
2021-03-12 18:09:43 -0800

    node class & basic math implementation

--- src/mcts/mcts.py
+++ src/mcts/mcts.py
@@ -1,5 +1,5 @@
 #monte carlo tree search for checkers ai
-
+# good pseudocode here: https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
 #algorithm:
 # 1. tree traversal to a leaf node.  go to child that maximizes UCB1(Si) 
 # 2. node expansion.  add more nodes to the tree using possible game actions
@@ -8,3 +8,23 @@
 # 
 # after x time or number of iterations, take the action that results in a higher average value
 # UCB1(Si) = v/ni + c * sqrt(ln(n0)/ni) = avg score at that state + exploration parameter (probably just 2) * sqrt((ln(total number of rollouts)/number of rollouts for this state)
+
+#imports
+import math
+
+#state node class
+class Node:
+    def __init__(self):
+        self.move = None #2 element array holding the move that leads to this state
+        self.t = 0 #total score
+        self.n = 0 #number of times visited
+        self.parent = None
+        self.children = []
+
+
+# basic ubc1 implementation
+def ubc1(v, ni, n0):
+    avg_score = v/ni
+    expl_constant = 2
+    result = avg_score + expl_constant * math.sqrt((math.log(n0)/ni))
+    return result
