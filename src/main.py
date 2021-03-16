db8aba086bfc60f9a4ba82a3056808370fafb00e
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2021-03-16 02:08:23 -0700

    add logging to .csv

--- src/main.py
+++ src/main.py
@@ -6,7 +6,11 @@ from checkers.game import Game
 
 from copy import deepcopy
 import random
+
+# Statistics gathering
+import datetime
 import time
+import csv
 
 def find_state(tree, board):
     for child in tree.children:
@@ -23,37 +27,93 @@ def custom_move_function(game, move):
     game.moves_since_last_capture = 0 if game.board.previous_move_was_capture else game.moves_since_last_capture + 1
 
 def main():
+    filename = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    csvfile = open(str(filename) + '.csv', 'w', newline='')
+    fieldnames = ['per game:', 'minimax win (1/0)',	'mcts win (1/0)', 'total time (s)',	'number of moves', 'minimax avg time per move (s)', 'mcts avg time per move (s)', 'minimax pieces left', 'mcts pieces left' ]  
+    header_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    header_writer.writeheader()
+    csvfile.close()
+    
+    for _ in range(0, 10):
+        with open(str(filename) + '.csv', 'a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            game_instance(writer)
+        
+def game_instance(csvlog_writer):
+
     game = Game()
 
-    window = GameWindow(800)
-    mcts_root = MCTSNode(game.board, None, None)
-    move_count = 0
+    # Set a conservative consecutive noncapture move limit to speed up gameplay
+    game.consecutive_noncapture_move_limit = 10
 
-    window.update(game.board)
+    # window = GameWindow(800)
+    mcts_root = MCTSNode(game.board, None, None)
+
+    mcts_move_count = 0
+    mcts_move_times = {}
+
+    minimax_move_count = 0
+    minimax_move_times = {}
+
+    # window.update(game.board)
+    start_time = time.time()
     while True:
-        window.clock.tick(60)
-        window.update(game.board)
+        #window.clock.tick(60)
+        #window.update(game.board)
         
         if game.move_limit_reached() or len(get_all_possible_moves(game.board)) == 0:
             break
         
         if game.whose_turn() == 1:
+            mcts_move_start_time = time.time()
             (mcts_child, mcts_move) = mcts(mcts_root)
+            mcts_turn_time = time.time() - mcts_move_start_time
+            mcts_move_times[mcts_move_count] = mcts_turn_time
+
             custom_move_function(game, mcts_move)
             mcts_root = mcts_child
-            print("MCTS chooses node with " + str(mcts_child.t) + "/" + str(mcts_child.n) + " score")
+            print("MCTS chooses node with " + str(mcts_child.t) + "/" + str(mcts_child.n) + " score in " + str(mcts_turn_time) + " sec")
+            mcts_move_count += 1
         
         else:
+            minimax_move_start_time = time.time()
             minimax_move = minimax(game, 0)
+            minimax_turn_time = time.time() - minimax_move_start_time
+            minimax_move_times[minimax_move_count] = minimax_turn_time
+
             custom_move_function(game, minimax_move)
             next_mcts_start = find_state(mcts_root, game.board)
             mcts_root = next_mcts_start if next_mcts_start != None else MCTSNode(game.board, None, None)
-            print("Minimax chooses a move")
-
-        move_count += 1
+            print("Minimax chose a move in " + str(minimax_turn_time) + " sec")
+            minimax_move_count += 1
     
+    end_time = time.time()
+
+
     winner = game.get_winner()
-    print("Game completed in " + str(move_count) + " moves. Winner is " + ("MCTS" if winner == 1 else "Minimax" if winner == 2 else "No one"))
+    print("Game completed in " + str(mcts_move_count + minimax_move_count) + " moves. Winner is " + ("MCTS" if winner == 1 else "Minimax" if winner == 2 else "No one"))
+
+    # statistics to be written to a csv file
+    minimax_win = 1 if winner == 2 else 0
+    mcts_win = 1 if winner == 1 else 0
+    elapsed_time = end_time - start_time
+    move_count = mcts_move_count + minimax_move_count
+    mcts_remaining_pieces = len(game.board.searcher.player_pieces[1])
+    minimax_remaining_pieces = len(game.board.searcher.player_pieces[2])
+    # Average mcts move time
+    mcts_average_move_time = 0
+    for i in range(0, mcts_move_count):
+        mcts_average_move_time += mcts_move_times[i]
+    mcts_average_move_time /= mcts_move_count
+    # Average minimax move time
+    minimax_average_move_time = 0
+    for i in range(0, minimax_move_count):
+        minimax_average_move_time += minimax_move_times[i]
+    minimax_average_move_time /= minimax_move_count
+    csvlog_writer.writerow({'minimax win (1/0)': minimax_win, 'mcts win (1/0)': mcts_win, 'total time (s)': elapsed_time, 'number of moves': move_count, 'minimax avg time per move (s)': minimax_average_move_time, 'mcts avg time per move (s)': mcts_average_move_time, 'minimax pieces left': minimax_remaining_pieces, 'mcts pieces left': mcts_remaining_pieces})
+
+
+    
 
 if __name__ == "__main__":
     main()
